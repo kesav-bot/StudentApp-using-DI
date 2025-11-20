07851a27b21b3ff6afcf0b7dd89fc2322cdc8783
Kesavan KG <kesavan.kg@vuedata.in>
2025-11-20 18:06:43 +0530

    added Depedency injection code

--- obj/Debug/net9.0/StudentApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d875cdfa58eabcaf34b8a86f80fc347bd92719a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c930e8aab566361bb8d132e2de594849db67a67b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
